e3d472d4674126410c9d5f170e789f09b7b833df
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-13 15:04:42 -0500

    easy_fix

--- src/Author.java
+++ src/Author.java
@@ -4,12 +4,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("all")
 public class Author extends Person {
-    private List books;
+
+    private  List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -29,7 +31,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
-        return String.format("%s, %s", lastName, firstName);
+    public String fullName() {
+        return String.format("%s, %s", firstName, lastName);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -5,7 +5,7 @@ public class Person {
     protected String firstName;
     protected String lastName;
 
-    public Person(String firstName, String lastName) {
+    Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
